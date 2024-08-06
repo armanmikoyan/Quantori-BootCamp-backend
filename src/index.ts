205990e4d1b4045b33d6b531307415f8dcfb99f8
armanmikoyan <arman.mikoyan1@gmail.com>
2024-08-06 18:51:35 +0400

    api documentation via swagger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import swaggerSpecs from "./swagger/config";
 import swaggerUi from "swagger-ui-express";
 import bodyParser from 'body-parser';
+import cors from "cors"
 import { mongoDbStart } from "./databases/mongodb";
 import { autoRoute } from "./routes/authRoute";
 import { profileRoute } from "./routes/profileRoute";
@@ -12,6 +13,7 @@ mongoDbStart();
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs)); // api documentation
 app.use("/", autoRoute); 
