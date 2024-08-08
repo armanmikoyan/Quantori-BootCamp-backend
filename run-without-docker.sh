e05f8881a06f29dd986ba7ee6e02c76b607bf286
armanmikoyan <arman.mikoyan1@gmail.com>
2024-08-08 19:00:47 +0400

    bash script for execution programm

--- run-without-docker.sh
+++ run-without-docker.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ "$(docker ps -q -f name=app-container)" ]; then
+   docker stop app-container
+fi
+
+if [ "$(docker ps -aq -f status=exited -f name=app-container)" ]; then
+   docker rm app-container
+fi
+
 cp .env.example .env
 npm install
 npm run build
