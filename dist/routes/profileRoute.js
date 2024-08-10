e2b5a22d86daaf912028acb23eb72ec7c2a28288
armanmikoyan <arman.mikoyan1@gmail.com>
2024-08-10 18:25:12 +0400

    docker file

--- dist/routes/profileRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.profileRoute = void 0;
-const express_1 = require("express");
-const profileController_1 = require("../controllers/profileController");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-exports.profileRoute = (0, express_1.Router)();
-const controller = new profileController_1.ProfileController();
-exports.profileRoute.get("/profile", authMiddleware_1.checkUserAuthentification, controller.getProfileInfo);
